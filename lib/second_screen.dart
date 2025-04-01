d0bc3524276ed31cd788d5c3fdbd5346c6dae3bb
TheBaytur <bayturcodding@gmail.com>
2025-04-01 15:12:59 +0800

    Second screen added

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; //It's my Second Screen
 
 class SecondScreen extends StatelessWidget {
   const SecondScreen({super.key});
